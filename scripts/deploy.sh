a494c42d12c7ebb969dddfe7dfaadc851a8d5074
Aaron Tay <psycokenisis@gmail.com>
2017-01-24 01:20:54 +1100

    Update distribution directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # Scripts to manage github pages deployment using a 'distribution' directory
 # Ref: http://www.damian.oquanta.info/posts/one-line-deployment-of-your-site-to-gh-pages.html
+# https://gist.github.com/cobyism/4730490
 #
 # This script assumes 'master' branch is the target deployment branch (i.e. user page)
 # (note: in the case of standard github pages, this is is 'gh-pages')
 # This script will deploy the CURRENT branch to the target branch
+set -o
+
 TARGET_DEPLOYMENT_BRANCH='master'
 DISTRIBUTION_DIRECTORY='dist'
 
